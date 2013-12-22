39208f0509e4e8b963321f649efb0d765c756bc9
Alan Storm <astorm@alanstorm.com>
2013-12-22 10:24:51 -0800

    Adding

---
+++ magento-tar-to-connect.better404.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+'base_dir'           => '/fakehome/Documents/github/MagentoBetter404/var/build',
+'archive_files'      => 'Pulsestorm_Better404.tar',
+'extension_name'     => 'Pulsestorm_Better404',
+'extension_version'  => '0.1.0',
+'archive_connect'    => 'Pulsestorm_Better404-0.1.0.tgz',
+'path_output'        => '/fakehome/Pulsestorm/var/build-connect',
+
+
+'stability'          => 'stable',
+'license'            => 'MIT',
+'channel'            => 'community',
+'summary'            => 'Gives programmers the 404 page they want.',
+'description'        => 'Replaces stock Magento 404 block with a "why isn\'t my routing working" diagnostic.',
+'notes'              => '',
+
+'author_name'        => 'Alan Storm',
+'author_user'        => 'alanstorm',
+'author_email'       => 'foo@example.com',
+'php_min'            => '5.2.0',
+'php_max'            => '6.0.0',
+'skip_version_compare' => false,
+);
